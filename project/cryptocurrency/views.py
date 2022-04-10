630a2838a66c6ce081ef35eee9aa35d55055b7dd
mickael anicette <mickaelanicette@gmail.com>
2022-04-10 20:19:52 -0300

    Add manage purchase page

--- project/cryptocurrency/views.py
+++ project/cryptocurrency/views.py
@@ -4,9 +4,10 @@ from .utils import (
     get_user_current_total_valorization,
 )
 from flask_login import login_required, current_user
-from .forms import PurchaseForm
+from .forms import PurchaseForm,QuickPurchaseForm
 from project import db
-from .models import Cryptocurrency,Purchase
+from .models import Cryptocurrency,Purchase,QuoteCurrency
+from sqlalchemy.sql import desc
 
 @crypto.route("/")
 @login_required
@@ -20,8 +21,16 @@ def home():
     )
 
 @crypto.route("/manage")
+@login_required
 def manage():
-    pass
+    purchase_historic = Purchase.query.filter(
+        Purchase.user_id == current_user.id
+    ).all()
+    return render_template(
+        "cryptocurrency/manage.html",
+        purchases=purchase_historic,
+        current_page="admin",
+    )
 
 
 @crypto.route("/add", methods=["GET", "POST"])
@@ -49,9 +58,34 @@ def add():
         current_page="add",
     )
 
-@crypto.route("/quick-add")
+@crypto.route("/quick-add", methods=["GET", "POST"])
+@login_required
 def quick_add():
-    pass
+    form = QuickPurchaseForm(request.form)
+    form.cryptocurrency.choices = [
+        (item.id, f"{item.symbol} ({item.name})")
+        for item in Cryptocurrency.query.all()
+    ]
+    if request.method == "POST" and form.validate():
+        last_quote = (
+            QuoteCurrency.query.filter(
+                QuoteCurrency.cryptocurrency_id == form.cryptocurrency.data
+            )
+            .order_by(desc(QuoteCurrency.date))
+            .first()
+        )
+        new_purchase = Purchase(
+            user_id=current_user.id,
+            cryptocurrency_id=form.cryptocurrency.data,
+            price=(last_quote.price * float(form.quantity.data)),
+            quantity=form.quantity.data,
+        )
+        db.session.add(new_purchase)
+        db.session.commit()
+        flash("Votre achats a bien été ajouté.", "success")
+    return render_template(
+        "cryptocurrency/add_edit.html", form=form, current_page="quick_add"
+    )
 
 
 @crypto.route("/edit/")

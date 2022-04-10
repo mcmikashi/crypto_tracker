d68a7be87f1f1f846c7802d7b036d0d3cabbf826
mickael anicette <mickaelanicette@gmail.com>
2022-04-10 12:52:48 -0300

    Add signup page

---
+++ project/authentification/forms.py
@@ -0,0 +1,46 @@
+from wtforms import (
+    Form,
+    StringField,
+    EmailField,
+    PasswordField,
+    validators,
+)
+
+
+class SignupForm(Form):
+
+    first_name = StringField(
+        "Prenom",
+        [validators.DataRequired(), validators.Length(2, 75)],
+        description="fa-solid fa-user",
+        render_kw={"placeholder": "Prenom"},
+    )
+    last_name = StringField(
+        "Nom",
+        [validators.DataRequired(), validators.Length(2, 75)],
+        description="fa-solid fa-user",
+        render_kw={"placeholder": "Nom"},
+    )
+    email = EmailField(
+        "E-mail",
+        [validators.DataRequired(), validators.Email()],
+        description="fa-solid fa-at",
+        render_kw={"placeholder": "E-mail"},
+    )
+    password = PasswordField(
+        "Mot de passe",
+        [
+            validators.DataRequired(),
+            validators.EqualTo(
+                "confirm", message="les mots de passe doivent correspondre"
+            ),
+            validators.Length(6, 150),
+        ],
+        description="fa-solid fa-lock",
+        render_kw={"placeholder": "Mot de passe"},
+    )
+    confirm = PasswordField(
+        "Répeter le Mot de passe",
+        description="fa-solid fa-lock",
+        render_kw={"placeholder": "Répeter le Mot de passe"},
+    )

57766cbef2a0fbda6c903597d7f013cec8412cd7
mickael anicette <mickaelanicette@gmail.com>
2022-04-10 12:52:48 -0300

    Add signup page

--- project/authentification/models.py
+++ project/authentification/models.py
@@ -1,2 +1,14 @@
-class User():
-    pass
+from project import db
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f"<User: {self.first_name} {self.last_name}>"

fb1191d2d12f058e7a5cd54e3e4e1f89e601d39b
mickael anicette <mickaelanicette@gmail.com>
2022-04-10 20:02:32 -0300

    Add add purchase page

---
+++ project/cryptocurrency/forms.py
@@ -0,0 +1,21 @@
+from wtforms import (
+    Form,
+    SelectField,
+    DecimalField,
+    validators,
+)
+
+
+class PurchaseForm(Form):
+    cryptocurrency = SelectField(
+        "Crypto monaie",
+        coerce=int,
+    )
+    price = DecimalField(
+        "Prix", [validators.DataRequired()], render_kw={"placeholder": "Prix"}
+    )
+    quantity = DecimalField(
+        "Quantité",
+        [validators.DataRequired()],
+        render_kw={"placeholder": "Quantité"},
+    )
--- project/cryptocurrency/views.py
+++ project/cryptocurrency/views.py
@@ -1,9 +1,12 @@
 from . import cryptocurrency_blueprint as crypto
-from flask import render_template
+from flask import render_template, request, flash
 from .utils import (
     get_user_current_total_valorization,
 )
 from flask_login import login_required, current_user
+from .forms import PurchaseForm
+from project import db
+from .models import Cryptocurrency,Purchase
 
 @crypto.route("/")
 @login_required
@@ -21,9 +24,30 @@ def manage():
     pass
 
 
-@crypto.route("/add")
+@crypto.route("/add", methods=["GET", "POST"])
+@login_required
 def add():
-    pass
+    form = PurchaseForm(request.form)
+    form.cryptocurrency.choices = [
+        (item.id, f"{item.symbol} ({item.name})")
+        for item in Cryptocurrency.query.all()
+    ]
+    if request.method == "POST" and form.validate():
+        new_purchase = Purchase(
+            user_id=current_user.id,
+            cryptocurrency_id=form.cryptocurrency.data,
+            price=form.price.data,
+            quantity=form.quantity.data,
+        )
+        db.session.add(new_purchase)
+        db.session.commit()
+        flash("Votre achats a bien été ajouté.", "success")
+    return render_template(
+        "cryptocurrency/add_edit.html",
+        form=form,
+        quick=True,
+        current_page="add",
+    )
 
 @crypto.route("/quick-add")
 def quick_add():

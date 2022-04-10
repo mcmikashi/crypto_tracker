b0004d278625f121ec627356de0b830d29062051
mickael anicette <mickaelanicette@gmail.com>
2022-04-10 13:12:48 -0300

    Add login page

--- project/__init__.py
+++ project/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = "authentification.login"
 
 
 def create_app(config_object=None):
@@ -17,6 +20,14 @@ def initialize_extensions(app):
     # Since the application instance is now created, pass it to each Flask
     # extension instance to bind it to the Flask application instance (app)
     db.init_app(app)
+    login_manager.init_app(app)
+    from .authentification.models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our 
+        # user table, use it in the query for the user
+        return User.query.get(int(user_id))
 
 def register_blueprints(app):
     # Since the application instance is now created, register each Blueprint
